ddda61eba3dd5f281832eca3ec338f4c9c3aacb0
Petulant Slacker <l.goohsen@gmail.com>
2017-04-14 20:24:27 +0200

    added threads to spawn 3 clients

--- src/de/htw/lenz/main/Main.java
+++ src/de/htw/lenz/main/Main.java
@@ -3,7 +3,26 @@ package de.htw.lenz.main;
 public class Main {
 
 	public static void main(String[] args) {
-		Client Mike = new Client("Luis");
+		Thread t1 = new Thread(new Task("Luis"));
+		t1.start();
+		Thread t2 = new Thread(new Task("Deschanel"));
+		t2.start();
+		Thread t3 = new Thread(new Task("Relana"));
+		t3.start();
 	}
 
+}
+
+final class Task implements Runnable {
+	
+	private String clientName;
+
+	public Task(String clientName) {
+		this.clientName = clientName;
+	}
+	@Override
+	public void run() {
+		Client client = new Client(this.clientName);
+	}
+	
 }
